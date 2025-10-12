11a77ab0e15f9fbdb31ae2054a2430745476eb84
Barbara-fs <barbara.fds@outlook.com>
2025-10-12 16:32:50 -0300

    Inserindo Leads

--- script.js
+++ script.js
@@ -6,26 +6,19 @@ function irParaAcessoVendas() {
   window.location.href = 'PaginaInicial.html';
 }
 
-function irParaLeads() {
-  window.location.href = 'leads.html';
-}
-
-function irParaPortifolio() {
-  window.location.href = 'portifolio.html';
-}
-
-function irstatusDaVenda() {
-  window.location.href = 'statusdavenda.html';
-}
-function irParaCashback() {
-  window.location.href = 'cashback.html';
-}
-function irParaPag() {
-  window.location.href = 'pag.html';
-}
+const Cadastro = document.getElementById('Cadastro');
+  Cadastro.innerHTML = '';
 
 
-button class btn btn 
 
-document.getelementByID('form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
+    document.getElementById('excelFile').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const data = e.target.result;
+                    document.getElementById('output').textContent = data;
+                };
+                reader.readAsText(file);
+            }
+        });
