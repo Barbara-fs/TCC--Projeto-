4a7bd88ede4573716b910c3e997541114775fc13
Barbara-fs <barbara.fds@outlook.com>
2025-10-16 22:35:37 -0300

    Incluindo paginas

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+
+
+
+function irParaInicio() {
+  window.location.href = 'inicio.html';
+
+
 function irParaOpções() {
   window.location.href = 'Opções.html';
 }
 
-function irParaAcessoVendas() {
-  window.location.href = 'PaginaInicial.html';
+function irParaVendas() {
+  window.location.href = 'Vendas.html';
 }
 
-const Cadastro = document.getElementById('Cadastro');
-  Cadastro.innerHTML = '';
+
+function irParaCashback() {
+  window.location.href = 'cashback.html';
+}
+
+function irParaCadastro() {
+  window.location.href = 'cadastro.html';
+}
+
+function irParaLeads() {
+  window.location.href = 'leads.html';
+}
+
+
+function irParaPag() {
+  window.location.href = 'pag.html';
+}
+
+function irParaPortifolio() {
+  window.location.href = 'portifolio.html';
+}
+
+function irParaStatusDaVenda() {
+  window.location.href = 'statusdavenda.html';
+}
+
+
+
+
+
+
+
+
+
+const Cadastro = document.getElementById('Cadastro.HTML');
+  Cadastro.innerHTML = 'output cadastro de clientes';
 
 
 
